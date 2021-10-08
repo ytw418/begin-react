21dbe6dbafc007eb548d81da265268fdd0176c27
ytw418 <ytw418@gmail.com>
2021-10-08 11:22:37 +0900

    라이프 사이클 추가 5의 배수 랜더링 x

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React,{Component} from 'react';
 import './App.css'
-import './MyName'
+import Counter from './Counter';
+
 import MyName from './MyName';
 
 class App extends Component {
   render() {
     return(
-     
+          <div>
             <MyName name="lcaros"></MyName>
+            <Counter></Counter>
+            </div>
     )
   }
 }
---
+++ src/Counter.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+
+class Counter extends Component{
+
+state = {
+    number : 0
+}
+
+//라이프 싸이클
+constructor(props) {
+    super(props);
+    console.log('constructor');
+  }
+  
+  componentWillMount() {
+    console.log('componentWillMount (deprecated)');
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    // 5 의 배수라면 리렌더링 하지 않음
+    console.log('shouldComponentUpdate');
+    if (nextState.number % 5 === 0) return false;
+    return true;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('componentWillUpdate');
+  }
+  
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate');
+  }
+//라이프 싸이클
+
+
+handleIncrease = () => {
+    this.setState({
+        number: this.state.number + 1
+    });
+}
+
+handleDecrease = () => {
+    this.setState({
+        number: this.state.number - 1
+    });
+}
+
+
+    render() {
+        return(
+            <div>
+                <h1>카운터</h1>
+                <div>값 : {this.state.number}</div>
+                <button onClick={this.handleIncrease}>+</button>
+                <button onClick={this.handleDecrease}>-</button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
